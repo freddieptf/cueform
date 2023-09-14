2431fcd601d0edc4984910c64c4bc97b10ca0da0
freddieptf <muiru@medic.org>
2023-09-14 16:53:57 +0300

    use a stateful decoder

--- cmd/cue2xlsform/main.go
+++ cmd/cue2xlsform/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/freddieptf/cueform/encoding/xlsform"
 )
@@ -12,6 +19,10 @@ var (
 	file      string
 	outputDir string
 )
+
+func writeFile(parentDir, file string, b []byte) error {
+	return ioutil.WriteFile(filepath.Join(parentDir, file), b, fs.ModePerm)
+}
 
 func main() {
 	flag.StringVar(&dir, "dir", "", "path to form directory with cue files")
@@ -26,9 +37,34 @@ func main() {
 		return
 	}
 	if file != "" {
-		err := xlsform.Decode(outputDir, file)
+		fReader, err := os.Open(file)
 		if err != nil {
 			log.Fatal(err)
+		}
+		decoder, err := xlsform.NewDecoder(fReader)
+		if err != nil {
+			log.Fatalf("err initializing decoder: %s", err)
+		}
+		result := decoder.Decode()
+		if result.Err != nil {
+			log.Fatal(err)
+		}
+		if outputDir == "stdout" {
+			fmt.Printf("%s\n\n===============================\n\n%s", result.Choices, result.Survey)
+		} else {
+			if outputDir == "" {
+				outputDir = filepath.Join("./", strings.TrimSuffix(filepath.Base(file), ".xlsx"))
+				if err = os.Mkdir(outputDir, fs.ModePerm); err != nil && !errors.Is(err, os.ErrExist) {
+					log.Fatalf("could not create output dir: %s", err)
+				}
+			}
+			if err = writeFile(outputDir, "choices.cue", result.Choices); err != nil {
+				log.Fatalf("err writing choices.cue: %s", err)
+			}
+			if err = writeFile(outputDir, "survey.cue", result.Survey); err != nil {
+				log.Fatalf("err writing survey.cue: %s", err)
+			}
+			fmt.Println("Done, files written to", outputDir)
 		}
 		return
 	}
--- encoding/xlsform/decode.go
+++ encoding/xlsform/decode.go
@@ -1,13 +1,9 @@
 package xlsform
 
 import (
-	"errors"
 	"fmt"
-	"io/fs"
-	"io/ioutil"
+	"io"
 	"log"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"cuelang.org/go/cue/ast"
@@ -17,28 +13,119 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-func makeFormDir(parent, formName string) (string, error) {
-	dir := filepath.Join(parent, formName)
-	if err := os.Mkdir(dir, fs.ModePerm); err != nil && !errors.Is(err, fs.ErrExist) {
-		return "", err
-	}
-	return dir, nil
+type Decoder struct {
+	r io.Reader
+	s state
 }
 
-func buildChoiceField(columnHeader []string, choiceKey string, rows [][]string) (*ast.StructLit, error) {
+func NewDecoder(r io.Reader) (*Decoder, error) {
+	s := &state{}
+	s, err := s.init()
+	if err != nil {
+		return nil, err
+	}
+	return &Decoder{r: r, s: *s}, nil
+}
+
+// experimental still, but at some point we'll need to use other schemas as long as they have definitions for Group,Choices and Question
+func (d *Decoder) UseSchema(module, pkg string) error {
+	d.s.module, d.s.pkg = module, pkg
+	importInfo, err := astutil.ParseImportSpec(ast.NewImport(nil, fmt.Sprintf("%s/%s", d.s.module, d.s.pkg)))
+	if err != nil {
+		return err
+	}
+	d.s.schemaImportInfo = importInfo
+	return nil
+}
+
+type DecodeResult struct {
+	Choices []byte
+	Survey  []byte
+	Err     error
+}
+
+func (d *Decoder) Decode() *DecodeResult {
+	file, err := excelize.OpenReader(d.r)
+	if err != nil {
+		return &DecodeResult{Err: err}
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+	sheets := file.GetSheetList()
+	for _, sheet := range []string{"survey", "choices", "settings"} {
+		if indexOf(sheets, sheet) == -1 {
+			return &DecodeResult{Err: fmt.Errorf("missing sheet %s", sheet)}
+		}
+	}
+	var (
+		choiceRows  [][]string
+		choiceBytes []byte
+		surveyRows  [][]string
+		surveyBytes []byte
+	)
+	if choiceRows, err = file.GetRows("choices"); err != nil {
+		return &DecodeResult{Err: err}
+	}
+	if fields, err := d.s.readChoiceSheet(choiceRows); err != nil {
+		return &DecodeResult{Err: err}
+	} else {
+		choiceBytes, err = d.s.getFileBytesFromFields(fields)
+		if err != nil {
+			return &DecodeResult{Err: err}
+		}
+	}
+	if surveyRows, err = file.GetRows("survey"); err != nil {
+		return &DecodeResult{Err: err}
+	}
+	if fields := d.s.readSurveySheet(surveyRows); err != nil {
+		return &DecodeResult{Err: err}
+	} else {
+		surveyBytes, err = d.s.getFileBytesFromNamedExpr(fields)
+		if err != nil {
+			return &DecodeResult{Err: err}
+		}
+	}
+	return &DecodeResult{Choices: choiceBytes, Survey: surveyBytes, Err: nil}
+}
+
+//state, nice
+type state struct {
+	module, pkg         string
+	schemaImportInfo    astutil.ImportInfo
+	surveyColumnHeaders []string
+	choiceColumnHeaders []string
+	nameColumnIdx       int
+	typeColumnIdx       int
+}
+
+func (s *state) init() (*state, error) {
+	s.module, s.pkg = "github.com/freddieptf/cueform", "schema/xlsform"
+	schemaImportInfo, err := astutil.ParseImportSpec(ast.NewImport(nil, fmt.Sprintf("%s/%s", s.module, s.pkg)))
+	if err != nil {
+		return nil, err
+	}
+	s.schemaImportInfo = schemaImportInfo
+	return s, nil
+}
+
+// builds a complete choice struct, requires only the rows describing one choice
+func (s *state) buildChoiceField(choiceKey string, rows [][]string) (*ast.StructLit, error) {
 	entries := &ast.ListLit{Rbrack: token.Newline.Pos()}
 	choice := ast.NewStruct(&ast.Field{Label: ast.NewIdent("list_name"), Value: ast.NewString(choiceKey)}, &ast.Field{Label: ast.NewIdent("choices"), Value: entries})
 	for _, row := range rows {
 		choiceEntry := &ast.Field{}
 		for idx, colVal := range row {
-			if columnHeader[idx] == "name" {
+			if s.choiceColumnHeaders[idx] == "name" {
 				choiceEntry.Label = ast.NewIdent(colVal)
-			} else if strings.HasPrefix(columnHeader[idx], "label::") {
+			} else if strings.HasPrefix(s.choiceColumnHeaders[idx], "label::") {
 				if choiceEntry.Value == nil {
 					choiceEntry.Value = ast.NewStruct()
 				}
 				choiceEntry.Value.(*ast.StructLit).Elts = append(choiceEntry.Value.(*ast.StructLit).Elts,
-					&ast.Field{Label: ast.NewIdent(strings.TrimPrefix(columnHeader[idx], "label::")), Value: ast.NewString(colVal)})
+					&ast.Field{Label: ast.NewIdent(strings.TrimPrefix(s.choiceColumnHeaders[idx], "label::")), Value: ast.NewString(colVal)})
 			}
 		}
 		entry := ast.NewStruct(choiceEntry)
@@ -48,20 +135,9 @@ func buildChoiceField(columnHeader []string, choiceKey string, rows [][]string) 
 	return choice, nil
 }
 
-func buildChoiceFile(pkg string, rows [][]string) (*ast.File, error) {
-	file := &ast.File{}
-	file.Decls = append(file.Decls, &ast.Package{Name: ast.NewIdent(pkg)})
-
-	schemaImportSpec := ast.NewImport(nil, "github.com/freddieptf/cueform/schema/xlsform")
-	file.Decls = append(file.Decls, &ast.ImportDecl{Specs: []*ast.ImportSpec{schemaImportSpec}})
-	info, err := astutil.ParseImportSpec(schemaImportSpec)
-	if err != nil {
-		log.Fatal(err)
-	}
-	schemaPkg := ast.NewIdent(info.Ident)
-
-	columnHeaders := rows[0]
-	listNameIdx := indexOf(columnHeaders, "list_name")
+// we return choices in the format map[list_name]array of rows specific to list_name
+func (s *state) buildChoiceMap(rows [][]string) map[string][][]string {
+	listNameIdx := indexOf(s.choiceColumnHeaders, "list_name")
 	choices := make(map[string][][]string)
 	for _, row := range rows[1:] {
 		if len(row) == 0 {
@@ -74,79 +150,90 @@ func buildChoiceFile(pkg string, rows [][]string) (*ast.File, error) {
 		choice = append(choice, row)
 		choices[row[listNameIdx]] = choice
 	}
+	return choices
+}
 
-	for choiceKey, rows := range choices {
-		choiceStruct, err := buildChoiceField(columnHeaders, choiceKey, rows)
+func (s *state) readChoiceSheet(rows [][]string) ([]*ast.Field, error) {
+	s.choiceColumnHeaders = rows[0]
+	fields := []*ast.Field{}
+	for choiceKey, rows := range s.buildChoiceMap(rows) {
+		choiceStruct, err := s.buildChoiceField(choiceKey, rows)
 		if err != nil {
 			return nil, err
 		}
-		file.Decls = append(file.Decls, &ast.Field{
+		fields = append(fields, &ast.Field{
 			Label: ast.NewIdent(choiceKey),
-			Value: ast.NewBinExpr(token.AND, &ast.SelectorExpr{X: schemaPkg, Sel: ast.NewIdent("#Choices")}, choiceStruct),
+			Value: ast.NewBinExpr(token.AND, &ast.SelectorExpr{X: ast.NewIdent(s.schemaImportInfo.Ident), Sel: ast.NewIdent("#Choices")}, choiceStruct),
 		})
 	}
-	return file, nil
+	return fields, nil
 }
 
-type field struct {
-	name      string
-	fieldType string
-	lit       *ast.StructLit
-}
-
-func buildQuestionStruct(columnHeaders []string, row []string) *ast.StructLit {
+func (s *state) buildQuestionStruct(nl bool, row []string) ast.Expr {
 	question := ast.StructLit{}
-	for idx, header := range columnHeaders {
+	for idx, header := range s.surveyColumnHeaders {
 		if idx >= len(row) || row[idx] == "" {
 			continue
 		}
 		question.Elts = append(question.Elts, &ast.Field{Label: ast.NewIdent(header), Value: ast.NewString(row[idx])})
 	}
-	return &question
+	importIdent := &ast.Ident{NamePos: token.Newline.Pos(), Name: s.schemaImportInfo.Ident}
+	if !nl {
+		importIdent = &ast.Ident{Name: s.schemaImportInfo.Ident}
+	}
+	return ast.NewBinExpr(token.AND, &ast.SelectorExpr{X: importIdent, Sel: ast.NewIdent("#Question")}, &question)
 }
 
-func buildGroupField(schemaPkg *ast.Ident, columnHeaders []string, total int, rows [][]string) (int, *field) {
-	typeColumnIdx := indexOf(columnHeaders, "type")
+type namedExpr struct {
+	name string
+	expr ast.Expr
+}
 
+// a group within a group within a group within a group
+func (s *state) buildGroupField(total int, rows [][]string) (int, *namedExpr) {
 	group := &ast.StructLit{}
 	groupRow := rows[0]
-	for idx, header := range columnHeaders {
+	for idx, header := range s.surveyColumnHeaders {
 		if idx >= len(groupRow) || groupRow[idx] == "" {
 			continue
 		}
 		group.Elts = append(group.Elts, &ast.Field{Label: ast.NewIdent(header), Value: ast.NewString(groupRow[idx])})
-
 	}
 	childrenList := &ast.ListLit{Rbrack: token.Newline.Pos()}
 	group.Elts = append(group.Elts, &ast.Field{Label: ast.NewIdent("children"), Value: childrenList})
-
 	for idx := 1; idx < len(rows); idx++ {
 		row := rows[idx]
 		if len(row) == 0 {
 			continue
 		}
-		if row[typeColumnIdx] == "begin group" {
-			fmt.Println("found nested", row[1])
-			nTotal, nested := buildGroupField(schemaPkg, columnHeaders, total, rows[idx:])
-			childrenList.Elts = append(childrenList.Elts, ast.NewBinExpr(token.AND, &ast.SelectorExpr{X: schemaPkg, Sel: ast.NewIdent("#Group")}, nested.lit))
+		if row[s.typeColumnIdx] == "begin group" {
+			nTotal, nested := s.buildGroupField(total, rows[idx:])
+			childrenList.Elts = append(childrenList.Elts, nested.expr)
 			idx += nTotal
 			total = idx
-		} else if row[typeColumnIdx] == "end group" {
+		} else if row[s.typeColumnIdx] == "end group" {
 			break
 		} else {
-			childStruct := buildQuestionStruct(columnHeaders, row)
-			childrenList.Elts = append(childrenList.Elts, ast.NewBinExpr(token.AND, &ast.SelectorExpr{X: schemaPkg, Sel: ast.NewIdent("#Question")}, childStruct))
+			childrenList.Elts = append(childrenList.Elts, s.buildQuestionStruct(true, row))
 			total++
 		}
 	}
-	return total, &field{name: groupRow[indexOf(columnHeaders, "name")], fieldType: "group", lit: group}
+	return total, &namedExpr{
+		name: groupRow[s.nameColumnIdx],
+		expr: ast.NewBinExpr(token.AND, &ast.SelectorExpr{X: ast.NewIdent(s.schemaImportInfo.Ident), Sel: ast.NewIdent("#Group")}, group),
+	}
 }
 
-func buildFields(schemaPkg *ast.Ident, columnHeaders []string, rows [][]string) []*field {
-	typeColumnIdx := indexOf(columnHeaders, "type")
-	idx, start := 0, -1
-	groupTrackz := []string{}
-	fields := []*field{}
+func (s *state) readSurveySheet(rows [][]string) []*namedExpr {
+	s.surveyColumnHeaders = rows[0]
+	s.typeColumnIdx = indexOf(s.surveyColumnHeaders, "type")
+	s.nameColumnIdx = indexOf(s.surveyColumnHeaders, "name")
+	var (
+		idx, start  = 0, -1
+		groupTrackz = []string{}
+		fields      = []*namedExpr{}
+	)
+	rows = rows[1:]
 	for {
 		if idx >= len(rows) {
 			break
@@ -156,25 +243,24 @@ func buildFields(schemaPkg *ast.Ident, columnHeaders []string, rows [][]string) 
 			idx++
 			continue
 		}
-		if row[typeColumnIdx] == "begin group" {
+		if row[s.typeColumnIdx] == "begin group" {
 			if start == -1 {
 				start = idx
 			}
 			groupTrackz = append(groupTrackz, "begin group")
-		} else if row[typeColumnIdx] == "end group" {
+		} else if row[s.typeColumnIdx] == "end group" {
 			if groupTrackz[len(groupTrackz)-1] == "begin group" {
 				groupTrackz = groupTrackz[:len(groupTrackz)-1]
 			}
 			if len(groupTrackz) == 0 {
-				fmt.Println("processing group:", rows[start][1])
-				_, group := buildGroupField(schemaPkg, columnHeaders, 0, rows[start:idx])
+				_, group := s.buildGroupField(0, rows[start:idx])
 				fields = append(fields, group)
 				start = -1
 			}
 		} else {
 			if start == -1 && len(row) > 0 {
 				// found rows not in a group, assume they are questions
-				fields = append(fields, &field{name: row[indexOf(columnHeaders, "name")], fieldType: "question", lit: buildQuestionStruct(columnHeaders, row)})
+				fields = append(fields, &namedExpr{name: row[s.nameColumnIdx], expr: s.buildQuestionStruct(false, row)})
 			}
 		}
 		idx++
@@ -182,100 +268,24 @@ func buildFields(schemaPkg *ast.Ident, columnHeaders []string, rows [][]string) 
 	return fields
 }
 
-func buildSurveyFile(pkg string, rows [][]string) (*ast.File, error) {
+func (s *state) getFileBytesFromFields(fields []*ast.Field) ([]byte, error) {
 	file := &ast.File{}
-	file.Decls = append(file.Decls, &ast.Package{Name: ast.NewIdent(pkg)})
-
-	schemaImportSpec := ast.NewImport(nil, "github.com/freddieptf/cueform/schema/xlsform")
-	file.Decls = append(file.Decls, &ast.ImportDecl{Specs: []*ast.ImportSpec{schemaImportSpec}})
-	info, err := astutil.ParseImportSpec(schemaImportSpec)
-	if err != nil {
-		log.Fatal(err)
-	}
-	schemaPkg := ast.NewIdent(info.Ident)
-	nlSchemaPkg := &ast.Ident{NamePos: token.Newline.Pos(), Name: info.Ident}
-
-	columnHeaders := rows[0]
-	for _, val := range buildFields(nlSchemaPkg, columnHeaders, rows[1:]) {
-		var field *ast.Field
-		if val.fieldType == "group" {
-			field = &ast.Field{
-				Label: ast.NewIdent(val.name),
-				Value: ast.NewBinExpr(token.AND, &ast.SelectorExpr{X: schemaPkg, Sel: ast.NewIdent("#Group")}, val.lit),
-			}
-		} else {
-			field = &ast.Field{
-				Label: ast.NewIdent(val.name),
-				Value: ast.NewBinExpr(token.AND, &ast.SelectorExpr{X: schemaPkg, Sel: ast.NewIdent("#Question")}, val.lit),
-			}
-		}
+	file.Decls = append(file.Decls,
+		&ast.Package{Name: ast.NewIdent("main")},
+		&ast.ImportDecl{Specs: []*ast.ImportSpec{ast.NewImport(nil, fmt.Sprintf("%s/%s", s.module, s.pkg))}})
+	for _, field := range fields {
 		file.Decls = append(file.Decls, field)
 	}
-
-	return file, nil
+	return format.Node(file, format.Simplify())
 }
 
-func writeFile(path, name string, file *ast.File) error {
-	out, err := format.Node(file, format.Simplify())
-	if err != nil {
-		return err
+func (s *state) getFileBytesFromNamedExpr(fields []*namedExpr) ([]byte, error) {
+	file := &ast.File{}
+	file.Decls = append(file.Decls,
+		&ast.Package{Name: ast.NewIdent("main")},
+		&ast.ImportDecl{Specs: []*ast.ImportSpec{ast.NewImport(nil, fmt.Sprintf("%s/%s", s.module, s.pkg))}})
+	for _, field := range fields {
+		file.Decls = append(file.Decls, &ast.Field{Label: ast.NewIdent(field.name), Value: field.expr})
 	}
-	return ioutil.WriteFile(filepath.Join(path, fmt.Sprintf("%s.cue", name)), out, fs.ModePerm)
-}
-
-func Decode(outDir, path string) error {
-	file, err := excelize.OpenFile(path)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			log.Println(err)
-		}
-	}()
-	sheets := file.GetSheetList()
-	for _, sheet := range sheets {
-		switch sheet {
-		case "survey":
-			fallthrough
-		case "choices":
-			fallthrough
-		case "settings":
-			fmt.Println("found", sheet)
-		default:
-			fmt.Println("unexpected sheet", sheet)
-		}
-	}
-	parentDir, err := makeFormDir(outDir, strings.TrimSuffix(filepath.Base(path), ".xlsx"))
-	if err != nil {
-		return err
-	}
-	choiceRows, err := file.GetRows("choices")
-	if err != nil {
-		return err
-	}
-	choiceFile, err := buildChoiceFile(filepath.Base(parentDir), choiceRows)
-	if err != nil {
-		return err
-	}
-	err = writeFile(parentDir, "choices", choiceFile)
-	if err != nil {
-		return err
-	}
-
-	surveyRows, err := file.GetRows("survey")
-	if err != nil {
-		return err
-	}
-
-	surveyFile, err := buildSurveyFile(filepath.Base(parentDir), surveyRows)
-	if err != nil {
-		return err
-	}
-	err = writeFile(parentDir, "survey", surveyFile)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return format.Node(file, format.Simplify())
 }
