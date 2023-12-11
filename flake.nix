75a8e3537dd1750b2acb6997f2a80abc99e5bf65
freddieptf <muiru@medic.org>
2023-12-11 03:38:03 +0300

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "cueform";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils"; 
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          buildGoApplication = gomod2nix.legacyPackages.${system}.buildGoApplication;       
+        in
+        {
+          packages.default = buildGoApplication {
+            pname = "cue2xlsform";
+            version = "0.1";
+            src = ./.;
+            subPackages = [ "cmd/cue2xlsform" ];
+            modules = ./gomod2nix.toml;
+          };
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              gopls
+              gotools
+              gomod2nix.packages.${system}.default
+           ];
+          };
+        })
+    );
+}
